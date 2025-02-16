2f8c3bf93496a42198f030c06cb15b2959f8d938
Rafieymehr <rafiey@gmail.com>
2025-02-16 15:21:21 -0500

    Project 4. Final Stage I1 1/16/2025 3:20

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const initialCards =
-  ({
+const initialCards = [
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -22,13 +22,43 @@ const initialCards =
   {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  });
+  },
+];
 
 let profileEditBotton = document.querySelector(".profile__edit-btn");
 let modalCloseBotton = document.querySelector(".modal__close-btn");
 let editProfileModal = document.querySelector(".modal");
+let formElement = document.querySelector(".modal__form");
+
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+
+let modalName = document.querySelector("#modal-name-input");
+let modalDescription = document.querySelector("#modal-description-input");
+
+let cardTemplate = document.querySelector("#card-template").content;
+
+let cardsList = document.querySelector("cards__list");
+
+function getCardElement(data) {
+  // cardTemplate is the content of the template element.
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  // cardElement is the element we want to add to the DOM.
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  // Populate the cloned element with card data.
+  cardElement.querySelector(".card__image").src = data.link;
+  cardElement.querySelector(".card__image").alt = data.name + "  Image";
+  cardElement.querySelector(".card__title").textContent = data.name;
+
+  return cardElement;
+}
 
 function openModal() {
+  modalName.value = profileName.textContent;
+  modalDescription.value = profileDescription.textContent;
+
   editProfileModal.classList.add("modal_opened");
 }
 
@@ -36,5 +66,18 @@ function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalName.value;
+  profileDescription.textContent = modalDescription.value;
+  closeModal();
+}
+
 profileEditBotton.addEventListener("click", openModal);
 modalCloseBotton.addEventListener("click", closeModal);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardslist = document.querySelector(".cards__list");
+  cardslist.append(getCardElement(initialCards[i]));
+}
